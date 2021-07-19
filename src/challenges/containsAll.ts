0e60514ff8086f233499882c146623668d552169
nickqweaver <nick.weaver@sendoutcards.com>
2021-07-19 11:43:16 -0600

    feat: added contains all challenge

---
+++ src/challenges/containsAll.ts
@@ -0,0 +1,19 @@
+/**
+ * Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+ * Example 1:
+ *   ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+ * Example 2:
+ *   The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+ * Example 3:
+ *   ["Alien", "line"], should return true because all of the letters in line are present in Alien.
+ * Example 4:
+ *   ["line", "Alien"], should return false because all of hte letters in Alien are not present in line
+ *
+ * @param arr -  an array of 2 strings
+ */
+
+export function containsAll(arr: [string, string]): boolean {
+  return false;
+}
+
+// This one is tricky, ask questions if needed ;)
