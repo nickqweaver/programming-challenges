2c2fa990afca5a23a4b1f600bcc0ee9cbefa8fd3
kparker028 <60025945+kparker028@users.noreply.github.com>
2021-08-04 17:08:33 -0600

    chore: thanks to some guidance was able to create a variable that holds all the values except for the friendsID's, via map

--- src/challenges/convertUsers.ts
+++ src/challenges/convertUsers.ts
@@ -11,20 +11,12 @@
 import { level3DB, OldUserType, NewUserType } from "./data/level3DataBase"; // level3DB is the data base you can use to perform lookups to find the friend ID's based on their names
 
 export const convertUsers = (users: OldUserType[]): NewUserType[] => {
-	const updateDate = (users: OldUserType) => {
-		const newUser = {
-			id: 0,
-			firstName: "",
-			lastName: "",
-			age: 0,
-			friendIDs: 0,
-		};
-		const shapeData = Object.create(newUser);
-		shapeData.id = users.id;
-		shapeData.firstName = users.first;
-		shapeData.lastName = users.last;
-		shapeData.age = users.age;
-		shapeData.friendIDs = users.id;
-	};
-	console.log(updateDate);
+	let reshape = users.map((user) => ({
+		id: user.id,
+		firstName: user.first,
+		lastName: user.last,
+		age: parseInt(user.age),
+	}));
+
+	console.log(reshape);
 };
