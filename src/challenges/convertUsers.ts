e90547465a08d79e1e68e62c34ab5f50f4b6aee2
kparker028 <60025945+kparker028@users.noreply.github.com>
2021-08-06 16:46:07 -0600

    chore: got a little bit of help from sawyer on slack but for the most part just spinning right, trying to figure how to utilize the names in the friends array to get the id's for those friends

--- src/challenges/convertUsers.ts
+++ src/challenges/convertUsers.ts
@@ -11,27 +11,56 @@
 import { level3DB, OldUserType, NewUserType } from "./data/level3DataBase"; // level3DB is the data base you can use to perform lookups to find the friend ID's based on their names
 
 export const convertUsers = (users: OldUserType[]): NewUserType[] => {
-	for (let i = 0; i < level3DB.length; i++) {
-		for (let k = 0; k < users.length; k++) {
-			// console.log(users[k]);
-			for (let i = 0; i < users[k].friends.length; i++) {
-				console.log(users[k].friends[i].split(" "), "user friends");
-			}
-		}
-		// for (let k = 0; k < level3DB[i].friends.length; k++) {
-		// 	let something = level3DB[i].friends[k];
-		// }
+	// for (let k = 0; k < level3DB[i].friends.length; k++) {
+	// 	let friendMatch =
+	// 		level3DB[i].friends[k] === users[k].friends[i] ? users[k].id : "";
 
-		// find the id based on either user.first or user.last
+	// find the id based on either user.first or user.last
 
-		let reshape = users.map((user, index) => ({
+	// for (let i = 0; i < level3DB.length; i++) {
+	// 	//console.log(level3DB[i].first,  "I am First");
+	// 	let dataFirstName = level3DB[i].first.toUpperCase().toString();
+	// 	console.log(dataFirstName);
+	// 	for (let k = 0; k < users.length; k++) {
+	// 		//console.log(users[k].first, "first name");
+	// 		for (let i = 0; i < users[k].friends.length; i++) {
+	// 			// console.log(users[k].friends[i].split(" "), "user friends");
+	// 			let userFriendList = users[k].friends[i].toUpperCase();
+	// 			console.log(userFriendList);
+	// 		}
+	// 	}
+	// }
+
+	let reshape = users.map((user, index) => {
+		const newFriend = user.friends.map(
+			(friend, index) => (friend = `${user.first} ${user.last}`)
+		);
+		const balls = {
 			id: user.id,
 			firstName: user.first,
 			lastName: user.last,
 			age: parseInt(user.age),
-			friendIDs: user.friends,
-		}));
+			friendIDs: newFriend === user.friends ? user.id : newFriend,
+		};
+		return balls;
+	});
 
-		// console.log(reshape);
-	}
+	console.log(reshape);
 };
+
+// let reshape = users.map((user, index) => ({
+
+// 	id: user.id,
+// 	firstName: user.first,
+// 	lastName: user.last,
+// 	age: parseInt(user.age),
+// 	friendIDs: user.friends.map((friend, index) =>
+
+// }));
+
+// console.log(reshape);
+//};
+
+// get the user
+// use user.friends array index and go back to old users array
+// match by using the frien name
