9eacef9417cfb5d551b0edb18e4c9c46f63b2633
kparker028 <60025945+kparker028@users.noreply.github.com>
2021-08-05 19:08:34 -0600

    chore: feel like I may be on to something

--- src/challenges/convertUsers.ts
+++ src/challenges/convertUsers.ts
@@ -11,21 +11,27 @@
 import { level3DB, OldUserType, NewUserType } from "./data/level3DataBase"; // level3DB is the data base you can use to perform lookups to find the friend ID's based on their names
 
 export const convertUsers = (users: OldUserType[]): NewUserType[] => {
-	// for (let i = 0; i < level3DB.length; i++) {
-	// 	for (let k = 0; k < level3DB[i].friends.length; k++) {
-	// 		console.log(level3DB[i].friends[k]);
-	// 	}
-	// }
+	for (let i = 0; i < level3DB.length; i++) {
+		for (let k = 0; k < users.length; k++) {
+			// console.log(users[k]);
+			for (let i = 0; i < users[k].friends.length; i++) {
+				console.log(users[k].friends[i].split(" "), "user friends");
+			}
+		}
+		// for (let k = 0; k < level3DB[i].friends.length; k++) {
+		// 	let something = level3DB[i].friends[k];
+		// }
 
-	// find the id based on either user.first or user.last
+		// find the id based on either user.first or user.last
 
-	let reshape = users.map((user, index) => ({
-		id: user.id,
-		firstName: user.first,
-		lastName: user.last,
-		age: parseInt(user.age),
-		friendIDs: user.friends === `${user.first} ${user.last}` ? user.id : "",
-	}));
+		let reshape = users.map((user, index) => ({
+			id: user.id,
+			firstName: user.first,
+			lastName: user.last,
+			age: parseInt(user.age),
+			friendIDs: user.friends,
+		}));
 
-	console.log(reshape);
+		// console.log(reshape);
+	}
 };
